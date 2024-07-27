aa52176134d8aaac17124b9520f3fa03059eab8f
CDamianS <A01656277@tec.mx>
2024-07-27 01:34:25 -0600

    Databse connection

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use App\Models\Job;
 
 Route::get('/', function () {
     return view('home');
@@ -9,23 +9,7 @@ Route::get('/', function () {
 
 Route::get('/jobs', function () {
     return view('jobs', [
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50000',
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10000',
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$4000',
-            ],
-        ],
+        'jobs' => Job::all(),
     ]);
 });
 
@@ -34,23 +18,6 @@ Route::get('/contact', function () {
 });
 
 Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-        [
-            'id' => 1,
-            'title' => 'Director',
-            'salary' => '$50000',
-        ],
-        [
-            'id' => 2,
-            'title' => 'Programmer',
-            'salary' => '$10000',
-        ],
-        [
-            'id' => 3,
-            'title' => 'Teacher',
-            'salary' => '$4000',
-        ],
-    ];
-    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+    $job = Job::find($id);
     return view('job', ['job' => $job]);
 });
